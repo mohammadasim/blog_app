364431dbcd5169aaf0a553a4a3979f4cefd11006
Mohammad Ayub <mohammad.ayub@imagination.com>
2021-03-29 08:50:45 +0100

    added template, css and pagination

--- blog_app/blogs/views.py
+++ blog_app/blogs/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Post
 
-# Create your views here.
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'post/list.html'
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request,
+                  'post/detail.html',
+                  {'post': post})
